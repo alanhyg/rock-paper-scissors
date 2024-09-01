1647e3d58a37bc31f4ffd9ac45c708306a04771b
alanhyg <alanhyg@gmail.com>
2024-09-01 16:28:23 +0800

    Create function to get computer choice randomly

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 // Generate random computer choice
+function getComputerChoice() {
+    let value = Math.random();
+    if (value < 1/3) {
+        return "rock";
+    } else if (value < 2/3) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
 
+console.log(getComputerChoice());
 
 // Prompt for user input
 
